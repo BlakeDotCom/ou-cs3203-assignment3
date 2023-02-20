081a2ddec93d795d3dba7e1255fb05595acd5106
Blake Austin <dot@Blakes-MacBook-Air.local>
2023-02-20 15:50:15 -0600

    Main method added

--- hw3.py
+++ hw3.py
@@ -10,4 +10,13 @@ def multiplyArray(numbers):
         ret = ret * x
     return ret
 
-    
+def main():
+    numbers = []
+    for x in range(5):
+        numbers.append(int(input("Enter a number: ")))
+    print(sumArray(numbers))
+    print(multiplyArray(numbers))
+
+
+if __name__ == "__main__":
+    main()
